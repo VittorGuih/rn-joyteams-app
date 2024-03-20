0558f8c39b2c846ca85267f7ef265e1022e90670
VittorGuih <vittytuuguilherme@gmail.com>
2024-03-19 21:21:40 -0300

    adiciona fonte e componente de carregamento

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
+
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    Roboto_400Regular, Roboto_700Bold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-      <ThemeProvider theme={theme}>
-        <Groups />
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      {
+        !fontsLoaded && !fontError ?
+        <Loading/> :
+        <Groups/>
+      }
+    </ThemeProvider>
   );
 }
 
