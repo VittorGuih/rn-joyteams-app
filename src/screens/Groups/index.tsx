71ac77eadba8b8845b68ed808a61b065b8ca0235
VittorGuih <vittytuuguilherme@gmail.com>
2024-03-21 18:55:06 -0300

    cria componente de cartao do grupo

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,18 @@
+import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
     return (
         <Container>
             <Header />
+            <Highlight 
+                title="Turmas"
+                subtitle="Jogue com a sua turma 2W"
+            />
+            <GroupCard title="Grupo 1"/>
+            <GroupCard title="Grupo 2"/>
         </Container>
     )
 }
