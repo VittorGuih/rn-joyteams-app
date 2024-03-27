00e33da75e2e7095e9e6facc070d4b42b4870f4e
VittorGuih <vittytuuguilherme@gmail.com>
2024-03-27 16:47:07 -0300

    usa componente de botao

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,18 +2,29 @@ import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList, Text } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { GroupList } from "@components/GroupList";
+import { Button } from "@components/Button";
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>([]);
+    const EMPTY_LIST_MESSAGE = "🎮 Que tal cadastrar a primeira turma? 🎮";
     return (
         <Container>
             <Header />
-            <Highlight 
+            <Highlight
                 title="Turmas"
-                subtitle="Jogue com a sua turma 2W"
+                subtitle="Jogue com a sua turma"
             />
-            <GroupCard title="Grupo 1"/>
-            <GroupCard title="Grupo 2"/>
+            <GroupList
+                data={groups}
+                keyExtractor={(item, index) => `${item}-${index}`}
+                renderItem={({ item, }) => <GroupCard title={item} />}
+                ListEmptyComponent={<ListEmpty message={EMPTY_LIST_MESSAGE} />}
+            />
+            <Button title="Criar nova turma"/>
         </Container>
     )
 }
-
